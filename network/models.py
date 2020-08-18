02e124696d0364005a1d4e3f8c2de708e3f00c30
minhajul-karim <minhajul.kaarim@gmail.com>
2020-08-18 20:23:33 +0600

    Create Posts model

--- network/models.py
+++ network/models.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Posts(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE)
+    time_posted = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.user_id} - {self.time_posted}"
