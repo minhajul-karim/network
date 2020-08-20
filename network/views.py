30ac794c384b56b3a09f9b9f3327a46c031b8ed6
minhajul-karim <minhajul.kaarim@gmail.com>
2020-08-20 10:54:11 +0600

    Change nav-item while not logged in

--- network/views.py
+++ network/views.py
@@ -44,7 +44,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse("login"))
 
 
 def register(request):
